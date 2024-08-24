bd04770c33177ce4bc413227b798e78943f389f4
Martin A. Ortega <lynxestudio@outlook.com>
2024-08-24 00:35:40 -0600

    Adds source code to the project

---
+++ src/Samples.Tutorial.TablaT/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Samples.Tutorial.TablaT
+{
+	class MainClass
+	{
+		public static int Main (string[] args)
+		{
+			Console.WriteLine ("+----------------------------+");
+			Console.WriteLine(" Logical AND (&&)");
+			Console.WriteLine(" F && F: " + (false && false));
+			Console.WriteLine(" F && T: " + (false && true));
+			Console.WriteLine(" T && F: " + (true && false));
+			Console.WriteLine(" T && T: " + (true && true));
+			Console.WriteLine("+------------------------------+");
+			Console.WriteLine(" Logical OR (||)");
+			Console.WriteLine(" F && F: " + (false || false));
+			Console.WriteLine(" F && T: " + (false || true));
+			Console.WriteLine(" T && F: " + (true || false));
+			Console.WriteLine(" T && T: " + (true || true));
+			Console.WriteLine("+------------------------------+");
+			Console.WriteLine(" Boolean logical AND (&)");
+			Console.WriteLine(" F & F: " + (0 & 0));
+			Console.WriteLine(" F & T: " + (0 & 1));
+			Console.WriteLine(" T & F: " + (1 & 0));
+			Console.WriteLine(" T & T: " + (1 & 1));
+			Console.WriteLine("+-------------------------------+");
+			Console.WriteLine(" Boolean logical inclusive OR (|)");
+			Console.WriteLine(" F | F: " + (0 | 0));
+			Console.WriteLine(" F | T: " + (0 | 1));
+			Console.WriteLine(" T | F: " + (1 | 0));
+			Console.WriteLine(" T | T: " + (1 | 1));
+			Console.WriteLine("+---------------------------------+");
+			Console.WriteLine(" Boolean logical exclusive OR (^)");
+			Console.WriteLine(" F ^ F: " + (0 ^ 0));
+			Console.WriteLine(" F ^ T: " + (0 ^ 1));
+			Console.WriteLine(" T ^ F: " + (1 ^ 0));
+			Console.WriteLine(" T ^ T: " + (1 ^ 1));
+			Console.WriteLine("+---------------------------------+");
+			Console.WriteLine(" Logical NOT (!) ");
+			Console.WriteLine(" !F: " + (!false));
+			Console.WriteLine(" !T: " + (!true));
+			Console.WriteLine("+---------------------------------+");
+			return 0;
+
+		}
+	}
+}
